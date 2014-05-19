79df83c5f0b6d00f0f939e8d030faac116d7a0db
CCrosbyweb <celeste@eagles.ewu.edu>
2014-05-19 13:41:51 -0700

    version1

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+
+  $('section').zoom({
+  	on:'grab'
+  });
+  // Your Scripts
+
+});
